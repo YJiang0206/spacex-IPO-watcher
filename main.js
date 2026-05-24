7eebebb948ef818033c0261425dfe5fd541cf6af
YJiang0206 <147996253+YJiang0206@users.noreply.github.com>
2026-05-24 14:25:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ async function checkIPO() {
   const shares = document.getElementById("shares").value;
   const status = document.getElementById("status");
 
-  status.innerText = "Checking backend...";
+  status.innerText = "Checking cloud backend...";
 
   try {
-    const response = await fetch("http://127.0.0.1:8000/check-ipo");
+    const response = await fetch(
+      "https://spacex-ipo-watcher.onrender.com/check-ipo",
+    );
     const data = await response.json();
 
     status.innerText = `${data.message} | ${ticker} | shares=${shares} | available=${data.available}`;
